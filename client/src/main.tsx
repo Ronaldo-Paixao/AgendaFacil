82a8963d539dab8141eba8c919d0d7a20bf30b89
Ronaldo <ronaldo.jrpaixao.rp@gmail.com>
2026-06-09 23:41:41 -0300

    correção url

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,14 +10,14 @@ function Root() {
   const [queryClient] = useState(() => new QueryClient());
 
   const [trpcClient] = useState(() =>
-    trpc.createClient({
-      links: [
-        httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
-        }),
-      ],
-    })
-  );
+  trpc.createClient({
+    links: [
+      httpBatchLink({
+        url: "/api/trpc",
+      }),
+    ],
+  })
+);
 
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
